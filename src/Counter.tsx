3992373469086f4263b6577c3705cce64d3dffb9
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-10-27 10:45:34 +0900

    more useRef

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -15,6 +15,10 @@ const Counter: React.FC<{}> = () => {
   useEffect(() => {
     renderTimes.current = renderTimes.current + 1;
   });
+  const ref = useRef<HTMLInputElement>(null!);
+  const focusInput = () => {
+    ref.current.focus();
+  };
 
   return (
     <div>
@@ -22,6 +26,8 @@ const Counter: React.FC<{}> = () => {
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
       <div>This component was re-rendered {renderTimes.current} times!</div>
+      <input ref={ref} type="text" />
+      <button onClick={focusInput}>Click Me!</button>
     </div>
   );
 };
